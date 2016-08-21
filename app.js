8942c4f8a9d91625ab9a22a971cd4728719cc2c9
Mat2001mat <mat_2001mat@hotmail.com>
2016-08-21 18:42:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express'),
  http = require('http'),
  path = require('path'),
- rpio = require('rpio');
+ gpio = require('pi-gpio'),
  app = express();
- 
- 
+ async = require('async'),
+
 //we use the port 3000
 app.set('port', 3000);
 app.get('/index1.jpg', new MjpegProxy('http://192.168.1.123:8080/?action=stream').proxyRequest);
@@ -40,55 +40,49 @@ var PiRobot = {
   
  //we open the gpio pins and set them as outputs
  init: function(){
-     rpio.open(this.motors.leftFront, rpio.OUTPUT, rpio.LOW);
-     rpio.open(this.motors.leftBack, rpio.OUTPUT, rpio.LOW);
-     rpio.open(this.motors.rightFront, rpio.OUTPUT, rpio.LOW);
-     rpio.open(this.motors.rightBack, rpio.OUTPUT, rpio.LOW);
+     gpio.open(this.motors.leftFront, "output");
+     gpio.open(this.motors.leftBack, "output");
+     gpio.open(this.motors.rightFront, "output");
+     gpio.open(this.motors.rightBack, "output");
  },
  
  //in order to move the tank forward, we supply both motors
  moveForward: function(){
-     rpio.write(this.motors.leftFront, rpio.HIGH);
-     rpio.write(this.motors.rightFront, rpio.HIGH);
-     rpio.write(this.motors.leftBack, rpio.LOW);
-     rpio.write(this.motors.rightBack, rpio.LOW);
-     console.log('Drive forward');
+   async.parallel([
+     console.log('Drive forward'),
+     gpio.write(this.motors.leftFront, 1),
+     gpio.write(this.motors.rightFront, 1)
+   ]);
  },
  
- //in order to move the tank backwards we supply the motors, but with inverse polarity
  moveBackward: function(){
-     rpio.write(this.motors.leftFront, rpio.LOW);
-     rpio.write(this.motors.rightFront, rpio.LOW);
-     rpio.write(this.motors.leftBack, rpio.HIGH);
-     rpio.write(this.motors.rightBack, rpio.HIGH);
-     console.log('Drive backwards');
+   async.parallel([
+     gpio.write(this.motors.leftBack, 1),
+     gpio.write(this.motors.rightBack, 1),
+     console.log('Drive backwards')
+   ]);
  },
  
- //in order to turn right, we supply the motor on the left
+  //in order to turn right, we supply the motor on the left
  moveLeft: function(){
-     rpio.write(this.motors.leftFront, rpio.LOW);
-     rpio.write(this.motors.rightFront, rpio.HIGH);
-     rpio.write(this.motors.leftBack, rpio.HIGH);
-     rpio.write(this.motors.rightBack, rpio.LOW);
-     console.log('Turn left');
+   gpio.write(this.motors.leftFront, 1);
+   console.log('Turn left');
  },
  
- //in order to turn left, we supply the motor on the right
+  //in order to turn left, we supply the motor on the right
  moveRight: function(){
-     rpio.write(this.motors.leftFront, rpio.HIGH);
-     rpio.write(this.motors.rightFront, rpio.LOW);
-     rpio.write(this.motors.leftBack, rpio.LOW);
-     rpio.write(this.motors.rightBack, rpio.HIGH);
-     console.log('Turn right');
+   gpio.write(this.motors.rightFront, 1);
+   console.log('Turn right');
  },
- 
- //in order to stop both motors, we set the all pins to 0 value
+
  stop: function(){
-     rpio.write(this.motors.leftFront, rpio.LOW);
-     rpio.write(this.motors.rightFront, rpio.LOW);
-     rpio.write(this.motors.leftBack, rpio.LOW);
-     rpio.write(this.motors.rightBack, rpio.LOW);
-     console.log('Stop');
+   async.parallel([
+     console.log('Stop'),
+     gpio.write(this.motors.leftFront, 0),
+     gpio.write(this.motors.leftBack, 0),
+     gpio.write(this.motors.rightFront, 0),
+     gpio.write(this.motors.rightBack, 0)
+   ]);
  }
 };
  
@@ -106,22 +100,22 @@ io.sockets.on('connection', function(socket) {
  socket.on('move', function(direction) {
    switch(direction){
      case 'up':
-       tank.moveForward();
+       PiRobot.moveForward();
        break;
      case 'down':
-       tank.moveBackward();
+       PiRobot.moveBackward();
        break;
      case 'left':
-       tank.moveLeft();
+       PiRobot.moveLeft();
        break;
      case 'right':
-       tank.moveRight();
+       PiRobot.moveRight();
        break;
    }
  });
  //we listen to the stop signal
  socket.on('stop', function(dir){
-   tank.stop();
+   PiRobot.stop();
  });
  
 });
