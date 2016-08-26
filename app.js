4f0e147e001182943bf1e88cde366c633026ac37
matt <mat_2001mat@hotmail.com>
2016-08-26 10:15:46 +0100

    -

--- app.js
+++ app.js
@@ -61,8 +61,6 @@ var PiRobot = {
     console.log('Drive forward');
     gpio.write(this.motors.leftFront, 1);
     gpio.write(this.motors.rightFront, 1);
-    gpio.write(this.motors.leftFront, 1);
-    gpio.write(this.motors.rightFront, 1);
 },
  
  moveBackward: function(){
